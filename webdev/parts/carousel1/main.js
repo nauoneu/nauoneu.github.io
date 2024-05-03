93c1eb1546f38cf606621ac2b2e1a15330704c85
nauoneu <nauoneu@gmail.com>
2024-05-03 12:59:46 +0900

    updated

--- webdev/parts/carousel1/main.js
+++ webdev/parts/carousel1/main.js
@@ -1,7 +1,9 @@
 $('.carousel').slick({
-  autoplay: true, // 自動再生
-  autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
-  arrows: false, // 矢印を無効
-  dots: true, // 左右のナビゲーションを無効
+  autoplay: true, // 自動切り替え
+  autoplaySpeed: 5000, // 切り替え速度(msec)
+  arrows: true, // 矢印
+  prevArrow: '<img src="../images/arrow_left.svg" class="slide-arrow prev-arrow">',
+  nextArrow: '<img src="../images/arrow_right.svg" class="slide-arrow next-arrow">',
+  dots: true, // ドット
   fade: true, // フェードで切り替え
 });
